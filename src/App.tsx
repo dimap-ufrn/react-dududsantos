5ae227faa049e768731c3c4909da292df6ddf9cb
eduardosantos <eduardo.victor.017@ufrn.edu.br>
2023-04-18 21:00:32 -0300

    finalizando o projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,175 +1,166 @@
 import './App.css'
-import { useState } from "react";
-import { Foods } from "./components/foods"
-import { foodType, foodCartType } from './types';
+import { useState } from 'react'
+import { Foods } from './components/foods'
+import { foodType, foodCartType } from './types'
+import styles from './index.module.scss'
 
 const products: foodType[] = [
   {
-    id: "1",
-    title: "Pizza da Mãe",
-    description: "Saborosa pizza caseira feita por sua mãe",
-    img: "https://www.sabornamesa.com.br/media/k2/items/cache/9189082f4804c1ab16e77d2cfe8d09d4_XL.jpg",
-    value: "49.99",
+    id: '1',
+    title: 'Pizza da Mãe',
+    description: 'Saborosa pizza caseira feita por sua mãe',
+    img: 'https://www.sabornamesa.com.br/media/k2/items/cache/9189082f4804c1ab16e77d2cfe8d09d4_XL.jpg',
+    value: '49.99'
   },
   {
-    id: "2",
-    title: "Churrasquinho",
-    description:
-      "Delicioso espetinho misto a moda da casa",
-    img: "https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia28136/churrasco-e-acompanhamentos-afe.jpg",
-    value: "65.80",
+    id: '2',
+    title: 'Churrasquinho',
+    description: 'Delicioso espetinho misto a moda da casa',
+    img: 'https://cptstatic.s3.amazonaws.com/imagens/enviadas/materias/materia28136/churrasco-e-acompanhamentos-afe.jpg',
+    value: '65.80'
   },
   {
-    id: "3",
-    title: "Espaguete",
-    description: "Massa ao molho de tomate",
-    img: "https://www.kitano.com.br/wp-content/uploads/2019/07/SSP_1756-Massa-spaguetti-ao-molho-sugo-e-manjerica%E2%95%A0%C3%A2o.jpg",
-    value: "34.90",
-  },
-];
-
-
+    id: '3',
+    title: 'Espaguete',
+    description: 'Massa fina italiana ao molho de tomate',
+    img: 'https://www.kitano.com.br/wp-content/uploads/2019/07/SSP_1756-Massa-spaguetti-ao-molho-sugo-e-manjerica%E2%95%A0%C3%A2o.jpg',
+    value: '34.90'
+  }
+]
 
 export default function App() {
-  const [cep, setCep] = useState<string>("");
+  const [cep, setCep] = useState<string>('')
   const [frete, setFrete] = useState({
     valor: 0.0,
-    endereco: "",
-  });
-  const [cart, setCart] =
-    useState<foodCartType[]>([
-      {
-        productId: "1",
-        quantity: 1,
-      },
-      {
-        productId: "2",
-        quantity: 1,
-      },
-      {
-        productId: "3",
-        quantity: 1,
-      },
-    ]);
+    endereco: ''
+  })
+  const [cart, setCart] = useState<foodCartType[]>([
+    {
+      productId: '1',
+      quantity: 1
+    },
+    {
+      productId: '2',
+      quantity: 1
+    },
+    {
+      productId: '3',
+      quantity: 1
+    }
+  ])
 
-  const numFoodCart = cart.reduce((accumulator, value) => accumulator + value.quantity,
+  const numFoodCart = cart.reduce(
+    (accumulator, value) => accumulator + value.quantity,
     0
-  );
+  )
 
-  const totalValue = cart.reduce((accumulator, value) =>
-    accumulator + Number(products.find((product) => product.id == value.productId)?.value) * value.quantity,
+  const totalValue = cart.reduce(
+    (accumulator, value) =>
+      accumulator +
+      Number(products.find(product => product.id == value.productId)?.value) *
+      value.quantity,
     0
   )
 
   const getCep = (cep: string) => {
-    fetch(`https://viacep.com.br/ws/${cep.replace("-", "")}/json`)
-      .then((res) => res.json())
-      .then((data) =>
-        setFrete((prev) => ({
+    fetch(`https://viacep.com.br/ws/${cep.replace('-', '')}/json`)
+      .then(res => res.json())
+      .then(data =>
+        setFrete(prev => ({
           valor: data.logradouro ? Math.random() * (30 - 11 + 2) + 8 : 0,
           endereco: data.logradouro
-            ? data.logradouro + " - " + data.bairro
-            : "CEP Inválido",
+            ? data.logradouro + ' - ' + data.bairro
+            : 'CEP Inválido'
         }))
-      );
+      )
   }
 
   const sendOrder = () => {
     const order = {
-      cart: cart.map((foodCart) => ({
-        product: products.find((product) => product.id == foodCart.productId)?.title,
-        quantity: foodCart.quantity,
+      cart: cart.map(foodCart => ({
+        product: products.find(product => product.id == foodCart.productId)
+          ?.title,
+        quantity: foodCart.quantity
       })),
       cep,
-      frete: frete.valor.toFixed(2),
+      frete: frete.valor.toFixed(2)
     }
-    console.log(order);
-    alert("Seu pedido foi enviado")
+    console.log(order)
+    alert('Seu pedido foi enviado')
   }
 
   return (
     <>
-
-      <main >
-        <div >
-          <div >
-            <h1 >Carrinho de compras</h1>
-            <span >
-              Você tem {numFoodCart}{" "}
-              {numFoodCart > 1 ? "itens" : "item"}
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.cart}>
+            <h1>Carrinho de compras</h1>
+            <span>
+              Você tem {numFoodCart} {numFoodCart > 1 ? 'itens' : 'item'}
             </span>
-            <div >
-              {cart.map((foodCart) => (
+            <div>
+              {cart.map(foodCart => (
                 <Foods
                   key={foodCart.productId}
                   quantity={foodCart.quantity}
                   setCart={setCart}
                   food={
                     products.find(
-                      (product) => product.id == foodCart.productId
+                      product => product.id == foodCart.productId
                     ) as foodType
                   }
                 />
               ))}
             </div>
           </div>
-          <div >
-            <h2 >Resumo</h2>
+          <div className={styles.resumo}>
+            <h2>Resumo</h2>
             <hr />
-            <span >Frete</span>
-            <div >
+            <span>Frete</span>
+            <div className={styles.cep}>
               <span>CEP</span>
-              <div >
+              <div className={styles.input}>
                 <input
                   name="cep"
                   value={cep}
-                  placeholder="Ex.: 00000-000"
+                  placeholder="00000-000"
                   maxLength={9}
-                  onChange={(e) => {
-                    let value = e.target.value;
-                    value = value.replace(/\D/g, "");
-                    value = value.replace(/(\d{5})(\d)/, "$1-$2");
-                    setCep(value);
-                    console.log(e.target.value.length);
+                  onChange={e => {
+                    let value = e.target.value
+                    value = value.replace(/\D/g, '')
+                    value = value.replace(/(\d{5})(\d)/, '$1-$2')
+                    setCep(value)
+                    console.log(e.target.value.length)
                     if (e.target.value.length == 9) {
-                      getCep(e.target.value);
+                      getCep(e.target.value)
                     }
                   }}
-
                 />
-                <button
-
-                  onClick={() => cep.length == 9 && getCep(cep)}
-                >
-                  <img
-                    src="https://www.svgrepo.com/show/506675/refresh.svg"
-
-                  />
+                <button onClick={() => cep.length == 9 && getCep(cep)}>
+                  <img src="https://www.svgrepo.com/show/506675/refresh.svg" />
                 </button>
               </div>
 
-              <span >R$ {frete.valor.toFixed(2)}</span>
+              <span>R$ {frete.valor.toFixed(2)}</span>
             </div>
-            <small >{frete.endereco}</small>
+            <small>{frete.endereco}</small>
             <hr />
-            <div >
+            <div className={styles.numItens}>
               <span>Itens ({numFoodCart})</span>
               <span>R$ {totalValue.toFixed(2)}</span>
             </div>
-            <div >
-              <span>Frete</span>
+            <div className={styles.frete}>
+              <span>Frete </span>
               <span>R$ {frete.valor.toFixed(2)}</span>
             </div>
             <hr />
-            <div >
-              <span>Total</span>
+            <div>
+              <span>Total </span>
               <span>R$ {(totalValue + frete.valor).toFixed(2)}</span>
             </div>
-            <button
+            <button className={styles.sendOrder}
               onClick={() => sendOrder()}
-              disabled={frete.endereco.length == 0 || numFoodCart == 0}
-
-            >
+              disabled={frete.endereco.length == 0 || numFoodCart == 0}>
               Fechar pedido
             </button>
           </div>
--- src/components/foods/index.tsx
+++ src/components/foods/index.tsx
@@ -1,83 +1,60 @@
-import { foodCartType, foodType } from "../../types"
+import { foodCartType, foodType } from '../../types'
+import styles from './styles.module.scss'
 
 interface foodProps {
-  food: foodType;
-  quantity: number;
-  setCart: React.Dispatch<React.SetStateAction<foodCartType[]>>;
-
+  food: foodType
+  quantity: number
+  setCart: React.Dispatch<React.SetStateAction<foodCartType[]>>
 }
 
 export const Foods = ({ food, quantity, setCart }: foodProps) => {
-
   const removeFood = () => {
-    setCart((prev) =>
-      prev.filter((foodCart) => foodCart.productId != food.id)
-    );
-  };
+    setCart(prev => prev.filter(foodCart => foodCart.productId != food.id))
+  }
 
-  const addOrSub = (action: "add" | "sub") => {
-    setCart((prev) => {
-      const cart = [...prev];
-      const index = cart.findIndex(
-        (foodCart) => foodCart.productId == food.id
-      );
-      if (action == "add") {
-        cart[index].quantity++;
+  const addOrSub = (action: 'add' | 'sub') => {
+    setCart(prev => {
+      const cart = [...prev]
+      const index = cart.findIndex(foodCart => foodCart.productId == food.id)
+      if (action == 'add') {
+        cart[index].quantity++
       } else {
         if (cart[index].quantity > 1) {
-          cart[index].quantity--;
+          cart[index].quantity--
         } else {
-          removeFood();
+          removeFood()
         }
+      }
 
-      };
-
-      return cart;
-
-    });
-  };
-
+      return cart
+    })
+  }
 
   return (
-    <div>
-      <img
-
-        src={food.img}
-      />
-      <div >
-        <span >{food.title} </span>
-        <small >
-          {food.description}
-        </small>
-        <span >R$ {food.value}</span>
+    <div className={styles.boxItems}>
+      <img className={styles.foodPic} src={food.img} />
+      <div className={styles.foodDetail}>
+        <span>{food.title} </span>
+        <small>{food.description}</small>
+        <span>R$ {food.value}</span>
       </div>
-      <div >
+      <div className={styles.foodActions}>
         <span>{quantity}</span>
-        <div >
-          <button onClick={() => addOrSub("add")}>
-            <img
-
-              src="https://cdn-icons-png.flaticon.com/512/4196/4196777.png"
-            />
+        <div className={styles.actions}>
+          <button onClick={() => addOrSub('add')}>
+            <img src="https://cdn-icons-png.flaticon.com/512/60/60564.png" />
           </button>
-          <button onClick={() => addOrSub("sub")}>
-            <img
-
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Circle_arrow_down_font_awesome.svg/1024px-Circle_arrow_down_font_awesome.svg.png"
-            />
+          <button onClick={() => addOrSub('sub')}>
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25623.png" />
           </button>
         </div>
-        <span >
+        <span className={styles.foodValue}>
           R${(quantity * Number(food.value)).toFixed(2)}
         </span>
         <button onClick={() => removeFood()}>
-          <img
-
-            src="https://cdn-icons-png.flaticon.com/512/860/860829.png"
-          />
+          <img src="https://cdn-icons-png.flaticon.com/512/860/860829.png" />
         </button>
       </div>
     </div>
   )
-
 }
